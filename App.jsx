388cd5e437e4514cdbda4e85416d9abc5c15f9a6
mohanprasaths <mohanprasath@live.com>
2017-02-01 12:18:14 +0530

    FB app

--- App.jsx
+++ App.jsx
@@ -5,6 +5,12 @@ class App extends React.Component {
       return (
          <div>
             Hello Worldss!!!
+            <div
+              class="fb-like"
+              data-share="true"
+              data-width="450"
+              data-show-faces="true">
+            </div>
          </div>
       );
    }
